2b16bb8cffcdf7a8157cd391c0c4551913585487
Shourov98 <63008046+Shourov98@users.noreply.github.com>
2024-07-15 17:31:18 +0600

    task-1 done

---
+++ module-6/task-1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+bool checkPalindrom(string &s, int n, int i) {
+    if(i == n) return true;
+    
+    if(s[n-1-i] != s[i]) return false;
+    
+    return checkPalindrom(s, n, i + 1);
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    int n = s.size();
+    
+    if(checkPalindrom(s, n, 0)) {
+        cout << "true";
+    } else {
+        cout << "false";
+    }
+
+    return 0;
+}
