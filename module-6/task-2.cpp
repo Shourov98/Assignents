dd3cf69c0e01120000acf5acb9632514e0e0ccb9
Shourov98 <63008046+Shourov98@users.noreply.github.com>
2024-07-15 17:32:03 +0600

    task-2 done

---
+++ module-6/task-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int rec_multiply(int mx,int mn,int mul) {
+    if(mn == 0) return mul;
+    mul += mx;
+    return rec_multiply(mx, mn-1, mul);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int mul = 0;
+    int mx = max(a, b);
+    int mn = min(a, b);
+    cout << rec_multiply(mx, mn, mul);
+    
+    return 0;
+}
